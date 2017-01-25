0f9ddfa01e22639b24b44036baefab7a484c4ae3
Administrator <gitlab.proit@gmail.com>
2017-01-25 18:16:26 +0530

    Update and change laouts

--- routes/web.php
+++ routes/web.php
@@ -11,73 +11,73 @@
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
-Route::get('/', function()
+Route::get('/admin', function()
 {
     return View::make('/admin/home');
 });
 
 Route::get('/admin/charts', function()
 {
-    return View::make('mcharts');
+    return View::make('/admin/mcharts');
 });
 
 Route::get('/admin/tables', function()
 {
-    return View::make('table');
+    return View::make('/admin/table');
 });
 
 Route::get('/admin/forms', function()
 {
-    return View::make('form');
+    return View::make('/admin/form');
 });
 
 Route::get('/admin/grid', function()
 {
-    return View::make('grid');
+    return View::make('/admin/grid');
 });
 
 Route::get('/admin/buttons', function()
 {
-    return View::make('buttons');
+    return View::make('/admin/buttons');
 });
 
 
 Route::get('/admin/icons', function()
 {
-    return View::make('icons');
+    return View::make('/admin/icons');
 });
 
 Route::get('/admin/panels', function()
 {
-    return View::make('panel');
+    return View::make('/admin/panel');
 });
 
 Route::get('/admin/typography', function()
 {
-    return View::make('typography');
+    return View::make('/admin/typography');
 });
 
 Route::get('/admin/notifications', function()
 {
-    return View::make('notifications');
+    return View::make('/admin/notifications');
 });
 
 Route::get('/admin/blank', function()
 {
-    return View::make('blank');
+    return View::make('/admin/blank');
 });
 
 Route::get('/admin/login', function()
 {
-    return View::make('login');
+    return View::make('/admin/login');
 });
 
 Route::get('/admin/documentation', function()
 {
-    return View::make('documentation');
+    return View::make('/admin/documentation');
 });
