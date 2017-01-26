267d682798447be3d60ae6b820bf3416a92a0121
Dinoosh Nikapitiya <dinoosh@proitsolutions.lk>
2017-01-26 19:11:12 +0530

    Update ui of user details page

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -14,7 +14,8 @@
     <!-- Stylesheets -->
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> -->
     <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset("assets/stylesheets/styles.css") }}" />
+    <link rel="stylesheet" href="{{ asset("assets/stylesheets/styles.css") }}"/>
+
 
 @yield('extra-css')
 
@@ -25,116 +26,55 @@
 
 <header>
     <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Brand</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#">Link</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="navbar-form navbar-left">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Link</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-    </nav>
-    <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                        aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    {{--<li class="{{ set_active('brands') }}"><a href="{{ url('/') }}">Home/Products</a></li>--}}
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+                </div>
 
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                            <i class="glyphicon glyphicon-user"></i> User <i class="glyphicon glyphicon-chevron-down"></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-user">
-                            <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                            </li>
-                            <li class="divider"></li>
-                            <li><a href="{{ url ('login') }}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                            </li>
-                        </ul>
-                    </li>
-                    {{--                    <li class="{{ set_active('cart') }}"><a href="{{ url('/User') }}"> User </a></li>--}}
-                    {{--                    <li class="{{ set_active('wishlist') }}"><a href="{{ url('/wishlist') }}">Wishlist--}}
-                    {{--                            ({{ Cart::instance('wishlist')->count(false) }})</a></li>--}}
-                    {{--<li class="{{ set_active('cart') }}"><a href="{{ url('/cart') }}">Cart--}}
-                    {{--({{ Cart::instance('default')->count(false) }})</a></li>--}}
-                </ul>
-            </div><!--/.nav-collapse -->
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-haspopup="true" aria-expanded="false">User <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href={{ url('brands') }}>Product List</a></li>
+                                <li><a href="#">Puchase History</a></li>
+                                <li><a href="#">Logout</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Logout</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
         </div>
     </nav>
-
 </header>
 
 @yield('content')
-
 <footer>
     <div class="container">
-        <p class="text-muted">By <a href="http://proitsolutions.lk">ProIT Solutions</a></p>
+
+        <div class="container">
+            <p class="text-muted">By <a href="http://proitsolutions.lk">ProIT Solutions</a></p>
+        </div>
     </div>
 </footer>
-
 <!-- JavaScript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script src="http://demo.startlaravel.com/sb-admin-laravel/assets/scripts/frontend.js" type="text/javascript"></script>
 @yield('extra-js')
 
 </body>
--- resources/views/sampath/user.blade.php
+++ resources/views/sampath/user.blade.php
@@ -1,24 +1,115 @@
-<!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
-<!--[if !IE]><!-->
-<html lang="en" class="no-js">
-<!--<![endif]-->
-<head>
-    <meta charset="utf-8"/>
-    <title>{{ config('app.admin', 'Laravel') }}</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta content="width=device-width, initial-scale=1" name="viewport"/>
-    <meta content="" name="description"/>
-    <meta content="" name="author"/>
+@extends('master')
 
-    <link rel="shortcut icon" href="{{ elixir('img/favicon.ico') }}">
-    <link rel="stylesheet" href="{{ elixir('css/admin_app.css') }}" />
-    <link rel="stylesheet" href="{{ asset("assets/stylesheets/styles.css") }}" />
-    <link rel="stylesheet" href="{{ asset("assets/stylesheets/styles.css") }}" />
-</head>
-<body>
-@yield('body')
-<script src="{{ asset("assets/scripts/frontend.js") }}" type="text/javascript"></script>
-</body>
-</html>
+@section('content')
+    <div class="container-fluid orange-background">
+        <div class="container white-background orange-gradiant-background">
+            <div class=" col-md-12 profile-head">
+                <div class="col-md- col-sm-4 col-xs-12">
+                    <img alt="Sampath Bank" src="img/sampath.jpg" width="209" hight="67" class="img-responsive"/>
+                </div><!--col-md-4 col-sm-4 col-xs-12 close-->
+
+
+                <div class="col-md-5 col-sm-5 col-xs-12">
+                    <h5>Sampath Bank</h5>
+                    <ul>
+                        <li><span class="glyphicon glyphicon-home"></span> Sampath Bank PLC, No 110 , Sir James Peiris
+                            Mawatha, Colombo 02, Sri Lanka.
+                        </li>
+                        <li><span class="glyphicon glyphicon-phone"></span> <a href="#" title="Phone">+94 11 2 30 30
+                                50</a>
+                        </li>
+                        <li><span class="glyphicon glyphicon-envelope"></span><a href="#" title="Email">info@sampath
+                                .lk</a>
+                        </li>
+
+                    </ul>
+
+
+                </div><!--col-md-8 col-sm-8 col-xs-12 close-->
+
+
+            </div>
+
+            <!-- Nav tabs -->
+            <div class="col-md-12">
+                <ul class="nav nav-tabs nav-menu" role="tablist">
+                    <li class="active">
+                        <a href="#agent" role="tab" data-toggle="tab">
+                            <i class="fa fa-male"></i>Agent Details
+                        </a>
+                    </li>
+
+                </ul>
+
+                <!-- Tab panes -->
+                <div class="tab-content white-background">
+                    <div class="tab-pane fade active in" id="agent">
+                        <div class="container">
+                            <div class="col-sm-11" style="float:left;">
+                                <br clear="all"/>
+                                <img alt="agent" src="https://diasp.eu/assets/user/default.png" width="100"
+                                     class="img-responsive"/><!--hve-pro close-->
+                                <!--col-sm-12 close-->
+                                <br clear="all"/>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h4 class="pro-title"></h4>
+                                    </div><!--col-md-12 close-->
+
+
+                                    <div class="col-md-6">
+
+                                        <div class="table-responsive responsiv-table">
+                                            <table class="table bio-table">
+                                                <tbody>
+                                                <tr>
+                                                    <td>Agent Name</td>
+                                                    <td>: Saman Perera</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Position</td>
+                                                    <td>: Sales Manager</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Address</td>
+                                                    <td>: 123, Blah Street, Blah blah, Colombo45, Sri Lanka</td>
+                                                </tr>
+
+
+                                                </tbody>
+                                            </table>
+                                        </div><!--table-responsive close-->
+                                    </div><!--col-md-6 close-->
+
+                                    <div class="col-md-6">
+
+                                        <div class="table-responsive responsiv-table">
+                                            <table class="table bio-table">
+                                                <tbody>
+                                                <tr>
+                                                    <td>Emai Id</td>
+                                                    <td>: saman@gmail.com</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Phone</td>
+                                                    <td>: (+94)112 456 789</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Mobile</td>
+                                                    <td>: (+94)73 4 567890</td>
+                                                </tr>
+
+                                                </tbody>
+                                            </table>
+                                        </div><!--table-responsive close-->
+                                    </div><!--col-md-6 close-->
+
+                                </div><!--row close-->
+                            </div><!--container close-->
+                        </div><!--tab-pane close-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
