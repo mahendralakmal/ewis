f424c7f076b3df16f9f13386fd53a83bb767bc8e
Administrator <gitlab.proit@gmail.com>
2017-01-27 05:24:01 +0530

    Added ewis logo to the menu

--- config/app.php
+++ config/app.php
@@ -10,9 +10,12 @@ return [
     | This value is the name of your application. This value is used when the
     | framework needs to place the application's name in a notification or
     | any other location as required by the application or its packages.
+
+        'name' => 'EWIS Peripherals',
+
     */
 
-    'name' => 'EWIS Peripherals',
+    'name' => '',
     'admin' => 'EWIS Peripherals Admin',
 
     /*
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -37,7 +37,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+                    <a class="navbar-brand" href="{{ url('/') }}"><img src="{{ elixir('img/ewis-logo.png') }}">{{ config('app.name', 'Laravel') }}</a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
