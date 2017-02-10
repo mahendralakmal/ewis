17933f54ea30e7ae3ff1a0f6f815481da90421d7
Mahendra Lakmal <mahendralakmal@Mahendras-MacBook-Pro.local>
2017-02-10 20:31:09 +0530

    Update ui of client side with media queries

--- resources/views/sampath/brands.blade.php
+++ resources/views/sampath/brands.blade.php
@@ -1,16 +1,15 @@
 @extends('master')
 
 @section('content')
-    <div class="container-fluid orange-background">
-        <div class="container white-background gradiant-background-white">
-        {{--<div class="container white-background gradiant-background">--}}
+    <div class="container-fluid profile-background">
+        <div class="container white-background gradiant-background">
             <div class=" col-md-12 profile-head">
                 <div class="col-md- col-sm-4 col-xs-12">
                     <img alt="Sampath Bank" src="{{ elixir('img/sampath.jpg') }}" width="209" hight="67" class="img-responsive"/>
                 </div><!--col-md-4 col-sm-4 col-xs-12 close-->
 
 
-                <div class="col-md-5 col-sm-5 col-xs-12 profile-head">
+                <div class="col-md-5 col-sm-8 col-xs-12 profile-head">
                     <div class="description">
                         <div class="inner">
                             <h3>Saman Perera</h3>
@@ -37,7 +36,7 @@
             </div>
 
             <!-- Nav tabs -->
-            <div class="col-md-12">
+            <div class="col-md-12 col-sm-12 col-sx-12 col-lg-12">
                 <ul class="nav nav-tabs nav-menu" role="tablist">
                     <li class="active">
                         <a href="#agent" role="tab" data-toggle="tab">
@@ -50,10 +49,10 @@
                 <!-- Tab panes -->
                 <div class="tab-content white-background">
                     <div class="tab-pane fade active in" id="agent">
-                        <div class="container col-md-12">
+                        <div class="container col-md-12 col-sm-12 col-sx-12 col-lg-12">
                             <div class="row">
                                 @foreach ($brands as $brand)
-                                    <div class="col-md-2 col-xs-2 col-sm-2 col-lg-2">
+                                    <div class="col-md-2 col-xs-12 col-sm-3 col-lg-2">
                                         <div class="thumbnail">
                                             <div class="caption text-center row">
                                                 <a href="{{ url('sampath/brands', [$brand->title,$brand->id]) }}">
