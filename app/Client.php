e936bc49b0a83e9244074814945462977029a7ad
Mahendra Lakmal <mahendralakmal@Mahendras-MacBook-Pro.local>
2017-02-12 23:13:12 +0530

    Login function partially completed

--- app/Client.php
+++ app/Client.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     protected $fillable=['user_id','name','address','telephone','email','logo','color','cp_name','cp_designation','cp_branch','cp_telephone','cp_email'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
