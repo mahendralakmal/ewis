25e3f5246832b2386faf66e8e6bcf7440c9c7c0a
Dinoosh Nikapitiya <dinoosh@proitsolutions.lk>
2017-02-14 10:30:59 +0530

    Bucket update banner

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -15,7 +15,7 @@
 
             <div class="col-md-8">
                 <h3>Rs.{{ $product->default_price }}</h3>
-                <form action="{{ route('product.AddToBucket', ['id' => $product->part_no ])}}" method="GET" class="side-by-side">
+                <form action="{{ route('product.AddToBucket', ['id' =>$product->id])}}" method="GET" class="side-by-side">
                     {{--{!! csrf_field() !!}--}}
                     <input type="hidden" name="id" value="{{ $product->part_no }}">
                     <input type="hidden" name="price" value="{{ $product->default_price }}">
