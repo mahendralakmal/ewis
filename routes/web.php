f3a2a4a0dbb64fdab64bf408435ff36137a35707
Mahendra Lakmal <mahendralakmal@Mahendras-MacBook-Pro.local>
2017-03-01 21:12:55 +0530

    Fixed admin dash bord displaying nonsense code

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,12 @@ Route::group(['prefix' => ' /client-profile'], function () {
     Route::get('/{id}/edit', 'ClientController@editClientProfile');
     Route::post('/{id}/postCheckout', 'BucketController@postCheckout');
     Route::get('/{id}/postCheckout', 'BucketController@postCheckout');
+
     Route::get('/{id}/brands', 'BrandsController@brands');
     Route::get('/{id}/{brand}/{brand_id}', 'CategoryController@category');
-    Route::get('/{id}/checkout', 'BucketController@Checkout');
     Route::get('/{id}/{brand}/{category}/{category_id}', 'ProductController@products');
+
+    Route::get('/{id}/checkout', 'BucketController@Checkout');
     Route::get('/{id}/{part_no}', 'ProductController@index');
     Route::post('/add-to-bucket', 'BucketController@getAddToBucket');
 });
