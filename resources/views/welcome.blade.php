0500cd85c7d78bfb37991cd1fe75ffdabfb9c8c8
Mahendra Lakmal <mahendralakmal@Mahendras-MBP.home>
2017-03-01 10:53:58 +0530

    Update header  titiles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>@yield('title', 'EWIS Peripherals')</title>
+
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Fonts -->
