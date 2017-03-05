49ff9409e00fbf503b168f2212d3238f5f3b4dbe
Mahendra Lakmal <mahendralakmal@Mahendras-MacBook-Pro.local>
2017-03-05 23:25:46 +0530

    Added error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,42 @@
+<html>
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				color: #ffffff;
+				display: table;
+				font-weight: 700;
+				font-family: 'Lato';
+				background-color: #ff5459;
+			}
+
+			.container {
+				text-align: center;
+				display: table-cell;
+				vertical-align: middle;
+			}
+
+			.content {
+				text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+				font-size: 72px;
+				margin-bottom: 40px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="content">
+				<div class="title">The page you are looking for cannot be displayed</div>
+			</div>
+		</div>
+	</body>
+</html>
