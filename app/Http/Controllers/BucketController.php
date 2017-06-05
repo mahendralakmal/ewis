b72a25fe07d2a1eed61c6177a2f543f587f37c65
Dinoosh Nikapitiya <dinoosh@proitsolutions.lk>
2017-06-05 10:36:09 +0530

    Fixed client history view

--- app/Http/Controllers/BucketController.php
+++ app/Http/Controllers/BucketController.php
@@ -306,7 +306,7 @@ class BucketController extends Controller
     public
     function getHistory()
     {
-        $orders = P_Order::find(User::find(Session::get('User'))->c_user->client_branch->id)->get();
+        $orders = P_Order::where('clients_branch_id',User::find(Session::get('User'))->c_user->client_branch->id)->get();
         if ($orders != null) {
             $orders->transform(function ($order, $key) {
                 $order->bucket = unserialize($order->bucket);
