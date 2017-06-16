e42c0045e1028a295450bb912a08dcb65a428e43
Mahendra Lakmal <mahendralakmal@Mahendras-MacBook-Pro.local>
2017-06-16 14:58:40 +0530

    change quentity control box size

--- resources/views/user/product.blade.php
+++ resources/views/user/product.blade.php
@@ -53,7 +53,7 @@
                                                     <p>{{ number_format($product->special_price,2,'.',',') }} </p>
                                                 </td>
                                                 <td><input type="number" value="1" name="Qty" id="Qty"
-                                                           class="col-lg-5 col-md-5 col-sm-5 col-xs-5"></td>
+                                                           class="form-controls"></td>
                                                 <td><input class="btn btn-success btn-sm" type="submit"
                                                            value="Add To Bucket"></td>
                                             </tr>
