ecc9818b86173e50ad1317405050aa37aadded93
Mahendra Lakmal <mahendralakmal@gmail.com>
2017-08-16 13:16:15 +0530

    Added new migration

---
+++ database/migrations/2017_08_16_064701_create_p_orders_history_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePOrdersHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('p_order_history', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('po_id');
+            $table->dateTime('po_datetime');
+//            $table->integer('clients_branch_id');
+//            $table->text('bucket');
+//            $table->text('cp_name');
+//            $table->text('cp_branch');
+//            $table->text('cp_telephone');
+//            $table->text('cp_address');
+//            $table->text('del_cp');
+//            $table->text('del_branch');
+//            $table->text('del_tp');
+//            $table->text('file')->nullable(true);
+//            $table->text('del_notes')->nullable(true);
+//            $table->text('del_address')->nullable(true);
+//            $table->text('cp_notes')->nullable(true);
+//            $table->integer('agent_id');
+            $table->text('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('p_order_history');
+    }
+}
